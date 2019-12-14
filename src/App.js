00d86907554196ffa4e5e34de4d421cf8e96fe32
ivanfslee <ivanfslee@gmail.com>
2019-12-13 18:16:13 -0800

    First commit - bootstrapped from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d'
+    axios.get(url).then((res) => {
+      console.log(res.data)
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Sanity sdfdfdfdf</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
